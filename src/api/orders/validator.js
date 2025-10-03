d4a1549ffe300209af71107cd55efdeb2c7b0629
Adhim Niokagi <niokagi@gmail.com>
2025-10-03 23:53:50 +0700

    create validator/types at orders module

--- src/api/orders/validator.js
+++ src/api/orders/validator.js
@@ -1,8 +1,12 @@
-import Joi from "joi";
+import Joi from 'joi';
 
-export const trxPayloadSchema = Joi.object({
-  id: Joi.string().required(),
-  user_id: Joi.string().required(),
-  product_id: Joi.string().required(),
-  //   add more
+const OrderItemSchema = Joi.object({
+  productId: Joi.number().integer().positive().required(),
+  quantity: Joi.number().integer().min(1).required(),
+});
+
+export const OrderPayloadSchema = Joi.object({
+  items: Joi.array().items(OrderItemSchema).min(1).required(),
+  customerName: Joi.string().allow('').optional(),
+  paymentMethod: Joi.string().valid('CASH', 'QRIS').required(),
 });
